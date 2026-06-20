8619875d1f2e5d4d0f2b394b6cc04d8eabffefce
wybxc <wybxc@qq.com>
2026-06-20 15:41:58 +0800

    Switch from system libgc to vendored bdwgc via cmake
    Build libgc statically from vendored/bdwgc source using the cmake crate,
    replacing system-deps lookup. Skip cmake on docs.rs for doc generation.

--- build.rs
+++ build.rs
@@ -1,15 +1,39 @@
 use std::path::PathBuf;
 
 fn main() {
-    let deps = system_deps::Config::new().probe().unwrap();
+    let is_docs_rs = std::env::var("DOCS_RS").is_ok();
 
-    let bindings = bindgen::Builder::default()
+    let dst = if !is_docs_rs {
+        let dst = cmake::Config::new("vendored/bdwgc")
+            .define("BUILD_SHARED_LIBS", "OFF")
+            .define("build_cord", "OFF")
+            .define("build_tests", "OFF")
+            .define("enable_docs", "OFF")
+            .define("enable_gcj_support", "OFF")
+            .define("enable_java_finalization", "OFF")
+            .define("enable_throw_bad_alloc_library", "OFF")
+            .define("enable_disclaim", "OFF")
+            .define("enable_handle_fork", "OFF")
+            .profile("Release")
+            .build();
+
+        println!("cargo:rustc-link-search=native={}/lib", dst.display());
+        println!("cargo:rustc-link-lib=static=gc");
+
+        Some(dst)
+    } else {
+        None
+    };
+
+    let mut builder = bindgen::Builder::default()
         .header("src/wrapper.h")
-        .clang_args(
-            deps.all_include_paths()
-                .iter()
-                .map(|path| format!("-I{}", path.to_string_lossy())),
-        )
+        .clang_arg("-Ivendored/bdwgc/include");
+
+    if let Some(ref dst) = dst {
+        builder = builder.clang_arg(format!("-I{}/include", dst.display()));
+    }
+
+    let bindings = builder
         .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
         .generate()
         .expect("Unable to generate bindings");
